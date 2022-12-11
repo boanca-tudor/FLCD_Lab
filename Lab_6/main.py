19e7661d25725a14d4d0da65f35645198019f822
boanca-tudor <tudorboanca@gmail.com>
2022-12-11 20:16:28 +0200

    Renamed lab folders

--- Lab_6/main.py
+++ Lab_6/main.py
@@ -1,22 +1,9 @@
+from Parser import Parser
 from grammar import Grammar
 
 if __name__ == '__main__':
-    grammar = Grammar.get_grammar_from_file("g1.txt")
-    print(grammar.get_terminals)
-    print(grammar.get_nonterminals)
-    print(grammar.get_productions)
-    print(grammar.get_starting_symbol)
+    grammar = Grammar.get_grammar_from_file("g3.txt")
+    parser = Parser(grammar)
+    # print(parser.parseSequence(['a', '*', '(', 'a', '+', 'a', ')']))
+    print(parser.parseSequence(['a', '*', '(', 'a', '+', 'a']))
 
-    print(grammar.get_productions_for_nonterminal("decllist"))
-
-    print(grammar.verify_CFG())
-
-    grammar = Grammar.get_grammar_from_file("g2.txt")
-    print(grammar.get_terminals)
-    print(grammar.get_nonterminals)
-    print(grammar.get_productions)
-    print(grammar.get_starting_symbol)
-
-    print(grammar.get_productions_for_nonterminal("expression"))
-
-    print(grammar.verify_CFG())
